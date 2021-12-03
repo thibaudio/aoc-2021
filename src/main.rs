422c059660f50d1ff95b6ceed91a7bafbd71ad5b
Thibaud Baas <thibaud.baas@dataiku.com>
2021-12-03 15:43:01 +0100

    Day2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod day1;
 use day1::Day1;
+mod day2;
+use day2::Day2;
+
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -13,6 +16,7 @@ fn main() {
     let args = Cli::from_args();
     match &args.day {
         1 => Day1::run(),
+        2 => Day2::run(),
         _ => panic!("Day not implemented"),
     }
 }
