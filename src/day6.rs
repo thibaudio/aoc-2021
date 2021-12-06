dc0f2071bdee0f2aec2aad726c2a8b4073c3b3db
Thibaud Baas <thibaud.baas@dataiku.com>
2021-12-06 21:57:26 +0100

    Day6

--- src/day6.rs
+++ src/day6.rs
@@ -8,43 +8,21 @@ pub struct Day6;
 
 impl Day6 {
     pub fn run() {
-        let mut school = load_input("./assets/day6.txt");
-        for _ in 0..80 {
-            simulate(&mut school);
-        }
-        print!("Day6 - Part1: {}\n", school.len());
+        let school = load_input("./assets/day6.txt");
+        print!("Day6 part1: {}\n", process(school.clone(), 80));
+        print!("Day6 part2: {}\n", process(school, 256));
     }
 }
 
-#[derive(Clone, Copy)]
-struct LanternFish {
-    timer: usize,
-}
-
-impl LanternFish {
-    fn tick(&mut self) -> Option<LanternFish> {
-        if self.timer == 0 {
-            let new_fish = LanternFish { timer: 8 };
-            self.timer = 6;
-            return Some(new_fish);
-        } else {
-            self.timer = self.timer - 1;
-            return None;
-        }
-    }
-}
-
-fn load_input(filename: impl AsRef<Path>) -> Vec<LanternFish> {
-    let mut input: Vec<LanternFish> = Vec::new();
+fn load_input(filename: impl AsRef<Path>) -> Vec<u128> {
+    let mut input = vec![0, 0, 0, 0, 0, 0, 0];
     if let Ok(lines) = read_lines(filename) {
         for line in lines {
             if let Ok(instruction) = line {
                 let fishes = instruction.split(',');
                 for f in fishes {
-                    let new_fish = LanternFish {
-                        timer: f.parse().unwrap(),
-                    };
-                    input.push(new_fish);
+                    let index = f.parse::<usize>().unwrap();
+                    input[index] = input[index] + 1;
                 }
             }
         }
@@ -52,34 +30,34 @@ fn load_input(filename: impl AsRef<Path>) -> Vec<LanternFish> {
     return input;
 }
 
-fn simulate(school: &mut Vec<LanternFish>) {
-    let mut to_add: Vec<LanternFish> = Vec::new();
-    for f in school.iter_mut() {
-        match f.tick() {
-            Some(new_fish) => to_add.push(new_fish),
-            None => (),
-        };
+fn process(mut school: Vec<u128>, days: usize) -> u128 {
+    let mut delayed = vec![0, 0, 0, 0, 0, 0, 0];
+    for d in 0..days {
+        let mod_day = d % 7;
+        school[((d + 2) % 7)] = school[((d + 2) % 7)] + delayed[mod_day];
+        delayed[mod_day] = school[mod_day];
     }
-    school.extend(to_add);
+    let mut count = 0;
+    for i in 0..school.len() {
+        count = count + school[i] + delayed[i];
+    }
+    return count;
 }
 
 #[test]
 fn test_day6_part1() {
-    let mut school = load_input("./assets/day6_test.txt");
-    for _ in 0..78 {
-        simulate(&mut school);
-    }
-    print!("{}", school.len());
-    assert_eq!(5934, school.len());
+    let school = load_input("./assets/day6_test.txt");
+    print!("{:?}", school);
+    let total = process(school, 80);
+    assert_eq!(5934, total);
 }
 
 #[test]
 fn test_day6_part2() {
-    let mut school = load_input("./assets/day6_test.txt");
-    for _ in 0..256 {
-        simulate(&mut school);
-    }
-    assert_eq!(26984457539, school.len());
+    let school = load_input("./assets/day6_test.txt");
+    print!("{:?}", school);
+    let total = process(school, 256);
+    assert_eq!(26984457539, total);
 }
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
